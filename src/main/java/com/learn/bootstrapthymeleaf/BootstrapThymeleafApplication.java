d0e888bcd858a93735906fe24c041201ebbfb613
Ish <ish.mishra@gmail.com>
2023-05-11 18:02:15 +0100

    Initial commit

---
+++ src/main/java/com/learn/bootstrapthymeleaf/BootstrapThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.learn.bootstrapthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapThymeleafApplication.class, args);
+	}
+
+}
